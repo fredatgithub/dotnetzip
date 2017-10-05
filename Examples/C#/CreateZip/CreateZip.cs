34a5f6deaf5e0cf36bc188385018dd4854855176
Fred <domaine75@gmail.com>
2017-10-05 17:32:11 +0200

    resharp code

--- Examples/C#/CreateZip/CreateZip.cs
+++ Examples/C#/CreateZip/CreateZip.cs
@@ -21,69 +21,70 @@
 //
 
 using System;
+using System.IO;
+using System.Net;
 using Ionic.Zip;
 
-namespace Ionic.Zip.Examples
+namespace CreateZip
 {
-    public class CreateZip
+  public class CreateZip
+  {
+    private static void Usage()
     {
-        private static void Usage()
-        {
-            Console.WriteLine("usage:\n  CreateZip <ZipFileToCreate> <directory>");
-            Environment.Exit(1);
-        }
-
-        public static void Main(String[] args)
-        {
-            if (args.Length != 2) Usage();
-            if (!System.IO.Directory.Exists(args[1]))
-            {
-                Console.WriteLine("The directory does not exist!\n");
-                Usage();
-            }
-            if (System.IO.File.Exists(args[0]))
-            {
-                Console.WriteLine("That zipfile already exists!\n");
-                Usage();
-            }
-            if (!args[0].EndsWith(".zip"))
-            {
-                Console.WriteLine("The filename must end with .zip!\n");
-                Usage();
-            }
-
-            string ZipFileToCreate = args[0];
-            string DirectoryToZip = args[1];
-            try
-            {
-                using (ZipFile zip = new ZipFile())
-                {
-                    // note: this does not recurse directories! 
-                    String[] filenames = System.IO.Directory.GetFiles(DirectoryToZip);
-
-                    // This is just a sample, provided to illustrate the DotNetZip interface.  
-                    // This logic does not recurse through sub-directories.
-                    // If you are zipping up a directory, you may want to see the AddDirectory() method, 
-                    // which operates recursively. 
-                    foreach (String filename in filenames)
-                    {
-                        Console.WriteLine("Adding {0}...", filename);
-                        ZipEntry e= zip.AddFile(filename);
-                        e.Comment = "Added by Cheeso's CreateZip utility."; 
-                    }
-
-                    zip.Comment= String.Format("This zip archive was created by the CreateZip example application on machine '{0}'",
-                       System.Net.Dns.GetHostName());
-
-                    zip.Save(ZipFileToCreate);
-                }
-
-            }
-            catch (System.Exception ex1)
-            {
-                System.Console.Error.WriteLine("exception: " + ex1);
-            }
-
-        }
+      Console.WriteLine("usage:\n  CreateZip <ZipFileToCreate> <directory>");
+      Environment.Exit(1);
     }
+
+    public static void Main(string[] args)
+    {
+      if (args.Length != 2) Usage();
+      if (!Directory.Exists(args[1]))
+      {
+        Console.WriteLine("The directory does not exist!\n");
+        Usage();
+      }
+      if (File.Exists(args[0]))
+      {
+        Console.WriteLine("That zipfile already exists!\n");
+        Usage();
+      }
+      if (!args[0].EndsWith(".zip"))
+      {
+        Console.WriteLine("The filename must end with .zip!\n");
+        Usage();
+      }
+
+      string zipFileToCreate = args[0];
+      string directoryToZip = args[1];
+      try
+      {
+        using (ZipFile zip = new ZipFile())
+        {
+          // note: this does not recurse directories! 
+          string[] filenames = Directory.GetFiles(directoryToZip);
+
+          // This is just a sample, provided to illustrate the DotNetZip interface.  
+          // This logic does not recurse through sub-directories.
+          // If you are zipping up a directory, you may want to see the AddDirectory() method, 
+          // which operates recursively. 
+          foreach (string filename in filenames)
+          {
+            Console.WriteLine("Adding {0}...", filename);
+            ZipEntry e = zip.AddFile(filename);
+            e.Comment = "Added by CreateZip utility.";
+          }
+
+          zip.Comment =
+            $"This zip archive was created by the CreateZip example application on machine '{Dns.GetHostName()}'";
+
+          zip.Save(zipFileToCreate);
+        }
+
+      }
+      catch (Exception ex1)
+      {
+        Console.Error.WriteLine("exception: " + ex1);
+      }
+    }
+  }
 }
